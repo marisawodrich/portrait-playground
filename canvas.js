72ae7a46e8a59803c8c2c53dce377517405e39c8
Marisa Wodrich <mwodrich@uni-osnabrueck.de>
2020-10-16 10:38:31 +0200

    added function for saving the canvas as png

--- canvas.js
+++ canvas.js
@@ -216,4 +216,24 @@ function animate() {
 
 }
 
+function exportCanvasAsPNG(id, fileName) {
+
+    var canvasElement = document.getElementById(id);
+
+    var MIME_TYPE = "image/png";
+
+    var imgURL = canvasElement.toDataURL(MIME_TYPE);
+
+    var dlLink = document.createElement('a');
+    dlLink.download = fileName;
+    dlLink.href = imgURL;
+    dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
+
+    document.body.appendChild(dlLink);
+    dlLink.click();
+    document.body.removeChild(dlLink);
+}
+
 animate();
+
+exportCanvasAsPNG('canvas',str('output'+str(selectedImage)));
